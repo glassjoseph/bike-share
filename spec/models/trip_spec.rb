bad86df60e6d7c290fedd98256771d65ce821015
glassjoseph <glassjoseph@gmail.com>
2017-03-22 18:59:51 -0600

    finished trip_spec

--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -80,15 +80,64 @@ RSpec.describe "A trip is created" do
     expect(Trip.count).to eq(1)
   end
 
-  it "a second created trip is saved in database" do
+  it "a multiple trips are saved in database" do
     time = DateTime.now
     station1 = Station.create(name: "Cloudbank", dock_count: 10, city_id: 1, installation_date: time)
     station2 = Station.create(name: "Bastion", dock_count: 1, city_id: 1, installation_date: time)
 
     Trip.create(id: 101, duration: 174, start_date: "2013-08-29 09:08:00", start_station: station1, end_date: "2013-08-29 09:11:00", end_station: station2, bike_id: 288, subscription_type: "Subscriber", zip_code: "94114")
-    expect(Trip.count).to eq(2)
+
+    Trip.create(id: 102, duration: 1740, start_date: "2013-08-29 09:08:00", start_station: station1, end_date: "2015-08-29 09:11:00", end_station: station2, bike_id: 288, subscription_type: "Subscriber", zip_code: "94114")
+
+    Trip.create(id: 103, duration: 2, start_date: "2013-08-30 09:08:00", start_station: station1, end_date: "2013-08-30 09:11:00", end_station: station2, bike_id: 7, subscription_type: "Subscriber", zip_code: "94114")
+
+    expect(Trip.count).to eq(4)
   end
 
+  it "a trip can show avg" do
+    expect(Trip.avg).to eq(522.5)
+  end
+
+  it "a trip can show longest" do
+    expect(Trip.longest).to eq(1740)
+  end
+
+  it "a trip can show shortest" do
+    expect(Trip.shortest).to eq(2)
+  end
+
+  it "a trip can show most_start" do
+    expect(Trip.most_start).to eq("Cloudbank")
+  end
+
+  it "a trip can show most_end" do
+    expect(Trip.most_end).to eq("Bastion")
+  end
+
+  it "a trip can show by_month" do
+    expect(Trip.by_month[:months][8]).to eq(4)
+    expect(Trip.by_month[:years][2013]).to eq(4)
+  end
+
+  it "a trip can show busiest_bike" do
+    expect(Trip.busiest_bike).to eq({288=>3})
+  end
+
+  it "a trip can show least_busy_bike" do
+    expect(Trip.least_busy_bike).to eq({7=>1})
+  end
+
+  it "a trip can show subscription_info" do
+    expect(Trip.subscription_info).to eq({:customers=>0, :subscribers=>4, :customers_percentage=>0.0, :subscribers_percentage=>100.0})
+  end
+
+  it "a trip can show busiest_day" do
+    expect(Trip.busiest_day[0].to_s).to eq("2013-08-29 00:00:00 UTC")
+  end
+
+  it "a trip can show least_busy_day" do
+    expect(Trip.least_busy_day[0].to_s).to eq("2013-08-30 00:00:00 UTC")
+  end
 
 end
 
